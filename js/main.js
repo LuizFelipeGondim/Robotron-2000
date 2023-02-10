ff4a766ea661a0a8f71f50e62ea51d8b4b523d1b
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2023-02-10 08:50:01 -0300

    fixed problem of 'negative' parts

--- js/main.js
+++ js/main.js
@@ -37,23 +37,35 @@ const pecas = {
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
-        atualizaEstatisticas(evento.target.dataset.peca)
+        let alterarValores = manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+
+        if (alterarValores) {
+            atualizaEstatisticas(evento.target.dataset.controle, evento.target.dataset.peca)
+        }
     })
 })
 
 function manipulaDados(operacao, controle) {
     const peca = controle.querySelector("[data-contador]")
 
-    if (operacao === "-") {
-        peca.value = parseInt(peca.value) - 1
-    } else {
+    if (operacao === "+") {
         peca.value = parseInt(peca.value) + 1
+        return true
+    } else if (operacao === "-" && peca.value > 0) {
+        peca.value = parseInt(peca.value) - 1
+        return true
+    } else {
+        return false
     }
 }
 
-function atualizaEstatisticas(peca) {
+function atualizaEstatisticas(operacao, peca) {
     estatisticas.forEach((elemento) => {
-        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        if (operacao === "-") {
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica]
+        } else {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+        }
+
     })
 }
